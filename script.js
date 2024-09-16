6a0e1fa538298079e190ad156716f230de1d7d16
somarza <145058100+somarza@users.noreply.github.com>
2024-09-15 22:07:16 -0400

    Push

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Your message has been sent!');
+    event.preventDefault(); // Prevent default form submission behavior
+
+    emailjs.sendForm('service_eguszal', 'template_gn6cm1t', this)
+        .then(function() {
+            alert('Your message has been sent successfully!');
+        }, function(error) {
+            alert('Failed to send the message. Please try again later.');
+        });
 });
